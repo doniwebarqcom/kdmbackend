4b2f7ace4c5f4d847eaf67cb2af0ad811cf70258
Ramdoni <doni.enginer@gmail.com>
2017-11-27 14:24:36 +0700

    Setting Themes

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="../plugins/images/favicon.png">
+    <title>@yield('title')</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Menu CSS -->
+    <link href="{{ asset('plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.css') }}" rel="stylesheet">
+    <!-- toast CSS -->
+    <link href="{{ asset('plugins/bower_components/toast-master/css/jquery.toast.css') }}" rel="stylesheet">
+    <!-- morris CSS -->
+    <link href="{{ asset('plugins/bower_components/morrisjs/morris.css') }}" rel="stylesheet">
+    <!-- chartist CSS -->
+    <link href="{{ asset('plugins/bower_components/chartist-js/dist/chartist.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('plugins/bower_components/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css') }}" rel="stylesheet">
+    <!-- Calendar CSS -->
+    <link href="{{ asset('plugins/bower_components/calendar/dist/fullcalendar.css') }}" rel="stylesheet" />
+    <!-- animation CSS -->
+    <link href="{{ asset('css/animate.css') }}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- color CSS -->
+    <link href="{{ asset('css/colors/green.css') }}" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="fix-header">
+
+    @section('sidebar')
+        This is the master sidebar.
+    @show
+
+    @yield('content')
+<!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="{{ asset('plugins/bower_components/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- Menu Plugin JavaScript -->
+    <script src="{{ asset('plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js') }}"></script>
+    <!--slimscroll JavaScript -->
+    <script src="{{ asset('js/jquery.slimscroll.js') }}"></script>
+    <!--Wave Effects -->
+    <script src="{{ asset('js/waves.js') }}"></script>
+    <!--Counter js -->
+    <script src="{{ asset('plugins/bower_components/waypoints/lib/jquery.waypoints.js') }}"></script>
+    <script src="{{ asset('plugins/bower_components/counterup/jquery.counterup.min.js') }}"></script>
+    <!--Morris JavaScript -->
+    <script src="{{ asset('plugins/bower_components/raphael/raphael-min.js') }}"></script>
+    <script src="{{ asset('plugins/bower_components/morrisjs/morris.js') }}"></script>
+    <!-- chartist chart -->
+    <script src="{{ asset('plugins/bower_components/chartist-js/dist/chartist.min.js') }}"></script>
+    <script src="{{ asset('plugins/bower_components/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js') }}"></script>
+    <!-- Calendar JavaScript -->
+    <script src="{{ asset('plugins/bower_components/moment/moment.js') }}"></script>
+    <script src="{{ asset('plugins/bower_components/calendar/dist/fullcalendar.min.js') }}"></script>
+    <script src="{{ asset('plugins/bower_components/calendar/dist/cal-init.js') }}"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="{{ asset('js/custom.min.js') }}"></script>
+    <script src="{{ asset('js/dashboard1.js') }}"></script>
+    <!-- Custom tab JavaScript -->
+    <script src="{{ asset('js/cbpFWTabs.js') }}"></script>
+    <script type="text/javascript">
+    (function() {
+        [].slice.call(document.querySelectorAll('.sttabs')).forEach(function(el) {
+            new CBPFWTabs(el);
+        });
+    })();
+    </script>
+    <script src="{{ asset('plugins/bower_components/toast-master/js/jquery.toast.js') }}"></script>
+    <!--Style Switcher -->
+    <script src="{{ asset('plugins/bower_components/styleswitcher/jQuery.style.switcher.js') }}"></script>
+</body>
+</html>
